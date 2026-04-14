cf225eb170ad3fc08fefa6a9dc88b6fe3a2f8567
Graham-J-Horan <grahamjhoran@gmail.com>
2026-04-14 18:44:44 +0200

    dummy comment to trigger rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+// Force fresh build
 export default defineConfig({
   output: "static",
   site: "https://web-craft.pages.dev", 
