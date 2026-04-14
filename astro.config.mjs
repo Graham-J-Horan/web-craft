996881ccc991982993d55f654d742bddf2636fdc
Graham-J-Horan <grahamjhoran@gmail.com>
2026-04-14 18:29:51 +0200

    changed base variable in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "static",
   site: "https://web-craft.pages.dev", 
-  base: "./", 
+  base: "/", 
   integrations: [tailwind()],
   build: {
     format: 'directory'
