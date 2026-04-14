71b30ef4e0f330f6e8190b80224402e485f3bdb8
Graham-J-Horan <grahamjhoran@gmail.com>
2026-04-14 11:56:00 +0200

    changed to direct api cal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   output: "static",
-  adapter: cloudflare({
-    imageService: "passthrough",
-    platformProxy: {
-      enabled: true,
-    },
-  }),
   integrations: [tailwind()],
 });
