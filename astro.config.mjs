57749770920a4ae5709d239e6983901008a11100
Graham-J-Horan <grahamjhoran@gmail.com>
2026-04-14 14:34:38 +0200

    fix global css import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   output: "static",
+  // Replace with your actual Cloudflare Pages URL once you have it
+  // site: "https://web-craft-v2.pages.dev", 
   integrations: [tailwind()],
+  build: {
+    format: 'directory'
+  }
 });
